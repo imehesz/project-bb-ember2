f0c876c466db5a238d8e27f7d4198cefdb725db7
Imre Mehesz <imehesz@imehesz-OptiPlex-990.(none)>
2015-10-29 12:06:34 -0400

    Initial commit with basic routes

---
+++ app/components/nav-menu.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+var navItems = [
+  {route:"index", label:"Home"}, 
+  {route:"moo", label:"Moo"},
+  {route:"book", label: "Books"}
+];
+
+var checkRoute = function(scope) {
+  if (!scope) scope = this;
+
+  var currentRoute = scope.container.lookup("controller:application").get("currentRouteName");
+  for(var i=0; i<scope.items.length; i++) {
+    var navRoute = scope.items[i];
+    Ember.set(navRoute, "isActive", false);
+    if (navRoute.route == currentRoute) {
+      Ember.set(navRoute, "isActive", true);
+    }
+  }
+};
+
+var NavMenuComponent = Ember.Component.extend({
+  init() {
+    this._super.apply(this, arguments);
+    checkRoute(this);
+  },
+  tagName: "nav",
+  items: navItems,
+  actions: {
+    checkRoute: checkRoute
+  }
+});
+
+export default NavMenuComponent;
