f37acf0c42fd0a1a26c58507c2a1dca860b9a79a
Imre Mehesz <imehesz@imehesz-OptiPlex-990.(none)>
2015-11-02 11:46:07 -0500

    Trying to add some data

--- app/routes/book.js
+++ app/routes/book.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      Ember.$.ajax("/data/books.json", {
+        success: (response) => {
+          resolve(response);
+        },
+        error: (reason) => {
+          reject(reason);
+        }
+      }) 
+    });
+  }
 });
