a811fc119236b4ab9e581d2c321021e15804f8e8
Imre Mehesz <imehesz@imehesz-OptiPlex-990.(none)>
2015-11-19 14:56:08 -0500

    Added  handling

--- app/routes/chapter.js
+++ app/routes/chapter.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return {
-      book: 1
-    }
+  model(o) {    
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      Ember.$.ajax("/data/verses.json?book=" + o.book_id + "&chapter=" + o.chapter_id + "&lang=asv", {
+        success: (response) => {
+          resolve({bookId: o.book_id, verses: response.verses});
+        },
+        error: (reason) => {
+          reject(reason);
+        }
+      }) 
+    });
   }
 });
